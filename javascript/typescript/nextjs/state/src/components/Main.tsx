a99712c63a7fe7d6ef39cc9beb3a22e839011ecb
jameskumpan <james@kumpan.se>
2025-12-30 20:24:22 +0100

    Adding state app using react context

---
+++ javascript/typescript/nextjs/state/src/components/Main.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React, { use, type ReactNode } from "react";
+import { UserContext } from "@/state/UserContext";
+
+export function Main({ children }: { children: ReactNode }) {
+  const { userName } = use(UserContext);
+  return (
+    <main>
+      <h1>Welcome</h1>
+      <p>{userName ? `Hello ${userName}!` : "Please sign in"}</p>
+      {children}
+    </main>
+  );
+}
